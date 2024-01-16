d05b04c54997851148469850505542fb7907c24c
Rodrigo Molina Rogé <rodrigaldi@gmail.com>
2024-01-16 00:33:12 -0300

    Rodri Version

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv("medical_examination.csv")
 
-# Add 'overweight' column
-df['overweight'] = None
+# Add
+df['overweight'] = np.where((df['weight'] / np.square(df['height']/100)) > 25, 1, 0)
 
-# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+# Normalize data by making 0 always good and 1 always bad.
+#f the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = np.where(df['cholesterol']>1, 1, 0)
+df['gluc'] = np.where(df['gluc']>1, 1, 0)
 
 # Draw Categorical Plot
 def draw_cat_plot():
-    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    # Create DataFrame for cat plot using `pd.melt`
+    # using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+
+    #Unpivot using pd.melt(frame, id_vars=None, value_vars=None, var_name=None, value_name='value', col_level=None, ignore_index=True)
+    df_cat = pd.melt (df, id_vars=['cardio'],value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature.
+
+    #Ex. DataFrame.groupby(by=None, axis=_NoDefault.no_default, level=None, as_index=True, sort=True, group_keys=True, observed=_NoDefault.no_default, dropna=True)[source]
+
+
+    #You will have to rename one of the columns for the catplot to work correctly.
+    df_cat["total"] = 1
+    df_cat = df_cat.groupby(["cardio", "variable","value"], as_index=False).count()
 
     # Draw the catplot with 'sns.catplot()'
 
-
-
-    # Get the figure for the output
-    fig = None
+    fig = sns.catplot(data=df_cat, x="variable", y="total", hue="value", kind="bar", col="cardio").fig
 
 
     # Do not modify the next two lines
@@ -38,20 +45,27 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))]
 
-    # Calculate the correlation matrix
-    corr = None
+
+    # Calculate the correlation matrix *Ex .corr(method='pearson', min_periods=1, numeric_only=False))
+    corr = df_heat.corr(method='pearson')
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(corr)
 
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12,12))
 
     # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr,linewidths=1, annot=True, square=True,mask=mask,fmt=".1f",center=0.08,cbar_kws={"shrink":0.5})
 
 
 
